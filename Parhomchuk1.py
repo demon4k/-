eb6b27f0b1caf9a312ece241babc9a880698d651
Dmytro Parhomchuk <86414517+demon4k@users.noreply.github.com>
2024-09-05 10:50:48 +0300

    Add files via upload

--- Parhomchuk1.py
+++ Parhomchuk1.py
@@ -1,9 +1,7 @@
-a = "Hello world"
-b = "Dmytro Parhomchuk"
+a = "Hello world!"
+b = "Dmytro Parhomchuk."
 d = 16
 
 print (a, type (a))
 print ("My name is", b, type (b))
-print ("I am", d, "years old", type (d))
-
-
+print ("I am", d, "years old.", type (d))
